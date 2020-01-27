c17637d4918c9c35d91721a492ee1d11a2a0747b
Pamela Aurora <44821924+aurorapamela@users.noreply.github.com>
2020-01-27 11:29:50 -0300

    Doing the exercise
    Doing the exercise

--- src/App.js
+++ src/App.js
@@ -2,6 +2,46 @@ import React from "react";
 import "./styles.css";
 
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      fecha: [
+        new Date().getDate(),
+        "/",
+        new Date().getMonth() + 1,
+        "/",
+        new Date().getFullYear()
+      ],
+      tiempo: [
+        new Date().getHours(),
+        ":",
+        new Date().getMinutes(),
+        ":",
+        new Date().getSeconds()
+      ],
+      reloj: setInterval(() => {
+        this.setState({
+          tiempo: [
+            new Date().getHours(),
+            ":",
+            new Date().getMinutes(),
+            ":",
+            new Date().getSeconds()
+          ]
+        });
+      }, 1000),
+      show: false,
+      btnTxtOff: "Mostrar fecha",
+      btnTxtOn: "Ocultar fecha"
+    };
+  }
+
+  mostrar() {
+    this.setState({
+      show: !this.state.show
+    });
+  }
+
   render() {
     return (
       <div>
@@ -20,8 +60,14 @@ class App extends React.Component {
             <div class="d-flex flex-row">
               <div class="col-md-4 mx-auto">
                 <div class="panel">
-                  <button type="button" class="btn btn-primary">
-                    Ocultar fecha
+                  <button
+                    onClick={() => this.mostrar()}
+                    type="button"
+                    class="btn btn-primary"
+                  >
+                    {this.state.show
+                      ? this.state.btnTxtOn
+                      : this.state.btnTxtOff}
                   </button>
                 </div>
               </div>
@@ -31,8 +77,10 @@ class App extends React.Component {
             <div class="d-flex flex-row">
               <div class="col-md-4 mx-auto">
                 <div class="display">
-                  <div class="display-time">16:34:42</div>
-                  <div class="display-date">14 de enero del 2020</div>
+                  <div class="display-time">{this.state.tiempo}</div>
+                  <div class="display-date">
+                    {this.state.show ? this.state.fecha : ""}
+                  </div>
                 </div>
               </div>
             </div>
